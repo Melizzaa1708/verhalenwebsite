82307a13b8ab46f1494f25980c80efd0e322df02
Melizzaa1708 <45657066+Melizzaa1708@users.noreply.github.com>
2018-12-07 12:37:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-document.getElementById("downloaden").addEventListener("click", function(){
-    var x = document.getElementById("downloadTekst");
+var downloadbutton = document.querySelector(".downloaden");
+var downloadTekst = document.querySelector("article p");
+var filter = document.querySelector("header form button");
+//console.log("button",downloadbutton)
+
+
+downloadbutton.addEventListener("click", function(){
+    var x = document.querySelector("article p");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
@@ -7,4 +13,24 @@ document.getElementById("downloaden").addEventListener("click", function(){
     }
 });
 
-console.log("test");
+filter.addEventListener("click", function(){
+    var x = document.querySelector("body form, div");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+});
+
+
+
+
+
+//!!
+
+//@Koop: "Dit stukje heb ik geprobeerd, maar werkt niet."
+
+downloadTekst.addEventListener("load", function(){
+    var x = document.querySelector("article p");
+        x.style.display = "none";
+});
