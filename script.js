4b727c685b199ed7e9956faad9e7b356440c19a4
Melizzaa1708 <45657066+Melizzaa1708@users.noreply.github.com>
2019-01-16 10:11:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+
 var filter = document.querySelector("header form button");
 var filterDicht = document.querySelector("body form button");
+var feedback = document.querySelector("article button:first-of-type");
+
+var mymarked = document.querySelectorAll("marked");
+var mystrong = document.querySelectorAll("strong");
+var mydel = document.querySelectorAll("del");
+var mysmall = document.querySelectorAll("small");
+
 
 filter.addEventListener("click", function(){
     var x = document.querySelector("body form");
@@ -12,13 +20,10 @@ filter.addEventListener("click", function(){
 
 filterDicht.addEventListener("click", function(){
     var x = document.querySelector("body form");
-    var y = document.querySelector("body div");
     if (x.style.display === "none") {
-        x.style.display = "block";
-        y.style.display = "block";
+       x.style.display = "block";
     } else {
         x.style.display = "none";
-        y.style.display = "none";
     }
 });
 
@@ -28,47 +33,79 @@ window.addEventListener("load", function(){
         x.style.display = "none";
 });
 
+//
 
-
-window.addEventListener("scroll", function(){
-    var mystrong = document.querySelectorAll("strong");
-    //console.log(mystrong)
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    //mystrong[1].className = "strong";
-    for(var i=0; i<mystrong.length;i++){
-        mystrong[i].className = "strong";
+feedback.addEventListener("click", function(){
+    var x = document.querySelector("aside");
+    if (x.style.display === "none") {
+       x.style.display = "block";
+    } else {
+        x.style.display = "none";
     }
-
-  } else {
-    for(var i=0; i<mystrong.length;i++){
-        mystrong[i].className = "";
-    }
-  }
-
 });
 
-window.addEventListener("scroll", function(){
-  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-    document.querySelector("small").className = "small";
-    document.querySelector("ins").className = "ins";
-  } else {
-    document.querySelector("small").className = "";
-      document.querySelector("ins").className = "";
-  }
+
+window.addEventListener("load", function(){
+    var x = document.querySelector("aside");
+        x.style.display = "none";
 });
 
+//
+
 window.addEventListener("scroll", function(){
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    document.querySelector("marked").className = "marked";
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    mystrong[0].className = "strong";
+
   } else {
-    document.querySelector("marked").className = "";
+    mystrong[0].className = "";
+
   }
 });
 
 window.addEventListener("scroll", function(){
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-    document.querySelector("del").className = "del";
+    if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+    mysmall[0].className = "small";
+    mymarked[0].className = "marked";
+
   } else {
-    document.querySelector("del").className = "";
+    mysmall[0].className = "";
+    mymarked[0].className = "";
   }
 });
+
+window.addEventListener("scroll", function(){
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    mydel[0].className = "del";
+  } else {
+    mydel[0].className = "";
+
+  }
+});
+
+window.addEventListener("scroll", function(){
+    if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+    mysmall[1].className = "small";
+  } else {
+    mysmall[1].className = "";
+  }
+});
+
+window.addEventListener("scroll", function(){
+    if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+    mymarked[1].className = "marked";
+  } else {
+    mymarked[1].className = "";
+  }
+});
+
+window.addEventListener("scroll", function(){
+  if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
+      mystrong[1].className = "strong";
+      mydel[1].className = "del";
+
+  } else {
+    mystrong[1].className = "";
+    mydel[1].className = "";
+  }
+});
+
